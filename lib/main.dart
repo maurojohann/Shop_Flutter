5b504225fad372a3a3a40ee41671238d9040a98b
Mauro Cezar Johann <maurojohann@gmail.com>
2020-11-24 17:12:50 -0300

    Utilizando InheritedWidget como gerencia de estado, acessando of(context) da classe criada.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'views/products_overview_screen.dart';
 import 'utils/app_routes.dart';
 import 'views/product_detail_screen.dart';
+import 'providers/couter_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.red[400],
-        fontFamily: 'Lato',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.red[400],
+          fontFamily: 'Lato',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../models/product.dart';
+import '../providers/couter_provider.dart';
 
-class ProductDetailScreen extends StatelessWidget {
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -10,6 +16,19 @@ class ProductDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Column(
+        children: [
+          Text(CounterProvider.of(context).state.value.toString()),
+          RaisedButton(
+              child: Text('+'),
+              onPressed: () {
+                print(CounterProvider.of(context).state.value);
+                setState(() {
+                  CounterProvider.of(context).state.inc();
+                });
+              })
+        ],
       ),
     );
   }
