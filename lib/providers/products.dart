e39989b7ff0c4edfc22390bfc1aaaf0b32ea1158
Mauro Cezar Johann <maurojohann@gmail.com>
2020-11-25 18:20:24 -0300

    Controlando o estado de isFavorite de forma global com Provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -4,8 +4,26 @@ import 'package:shop/models/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = DUMMY_PRODUCTS;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((element) => element.isFavorite).toList();
+    }
+    {
+      return [..._items];
+    }
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha LOja'),
+        actions: [
+          PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.Favorite) {
+                  products.showFavoriteOnly();
+                } else {
+                  products.showAll();
+                }
+              },
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (_) => [
+                    PopupMenuItem(
+                      child: Text('Somente Favoritos'),
+                      value: FilterOptions.Favorite,
+                    ),
+                    PopupMenuItem(
+                      child: Text('Todos'),
+                      value: FilterOptions.All,
+                    )
+                  ])
+        ],
       ),
       body: ProductGrid(),
     );
