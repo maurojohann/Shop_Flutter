f558c981c07fbb6cdcc81957825727abf057c644
Mauro Cezar Johann <maurojohann@gmail.com>
2020-11-25 17:24:49 -0300

    Usando Consumer

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,47 +7,49 @@ import '../models/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Product product = Provider.of<Product>(context);
+    // final Product product = Provider.of<Product>(context);
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            //Navegacao sendo necessario nomear as rotas
-            Navigator.of(context)
-                .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
+    return Consumer(
+      builder: (ctx, product, _) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              //Navegacao sendo necessario nomear as rotas
+              Navigator.of(context)
+                  .pushNamed(AppRoutes.PRODUCT_DETAIL, arguments: product);
 
-            //Navigacao sem a necessidade de nomear as rotas
-            //Navigator.of(context).push(MaterialPageRoute(
-            //  builder: (ctx) => ProductDetailScreen(product),
-            // ));
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+              //Navigacao sem a necessidade de nomear as rotas
+              //Navigator.of(context).push(MaterialPageRoute(
+              //  builder: (ctx) => ProductDetailScreen(product),
+              // ));
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-              color: Theme.of(context).accentColor,
-              //iconSize: 18,
-              icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              ),
-              onPressed: product.toggleFavorite),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.left,
-            overflow: TextOverflow.clip,
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+                color: Theme.of(context).accentColor,
+                //iconSize: 18,
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                ),
+                onPressed: product.toggleFavorite),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.left,
+              overflow: TextOverflow.clip,
+            ),
+            trailing: IconButton(
+                iconSize: 18,
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                ),
+                onPressed: () {}),
           ),
-          trailing: IconButton(
-              iconSize: 18,
-              icon: Icon(
-                Icons.add_shopping_cart,
-              ),
-              onPressed: () {}),
         ),
       ),
     );
